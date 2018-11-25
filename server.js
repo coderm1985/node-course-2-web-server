e727042ee3c4c102a9447951c232fb932fa71b23
coderm1985 <coderm1985@gmail.com>
2018-11-25 19:28:49 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+"/views/partials");
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+});
+app.set('view engine','hbs');
+
+app.use(express.static(__dirname+'/public'));
+
+app.use((req,res,next)=>{
+    var log =`Request method: ${req.method}   Request Url:${req.url}`;
+    fs.appendFile('server.log',log+'\n',(err)=>{
+        if(err){
+            console.log('Unable to create file server.log');
+            
+        }
+    })
+    next();
+});
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        pageTitle:"Home Page here",
+        likings:[
+            "Music",
+            "Programming"
+        ],
+        welcomeMessage:"Welcome to Home",
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle:'About Us',
+        fullYear: new Date().getFullYear()
+    });
+});
+
+app.listen(3000);
