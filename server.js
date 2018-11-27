d610582eaa2f24a70f0f3ba6ad8eed3e454d4b7c
coderm1985 <coderm1985@gmail.com>
2018-11-27 20:43:33 +0530

    port parameter changed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 var app = express();
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname+"/views/partials");
 hbs.registerHelper('getCurrentYear',()=>{
